59fe33867c1b42a773fea84484fa7706411ee3e3
Kerry flowers <114622964+kflowers-pvcc@users.noreply.github.com>
2022-11-14 20:03:05 -0500

    Add files via upload

---
+++ magic8.py
@@ -0,0 +1,12 @@
+# Name: Kerry Flowers
+#   Prog Purpose: This Magic 8-ball code uses a Python tuple since the
+#   user does not have the ability to change the 8-ball answers.
+#   However, the program could have used a Python list instead of a tuple.
+#   NOTE: Tuplets use parentheses; lists use square braces.
+
+import random
+answers_8_ball = ( "As I see it, yes", "Ask again later",
+    "Better not tell you now", "Cannot predict now",
+    "Concentrate and ask again", "Don't count on it",
+    "It is certain", "It is decidedly so"
+                   
